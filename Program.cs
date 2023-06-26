3e0da428632eecce3416c67b0ac0e656501aaa9a
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-26 12:46:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Последовательность
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Я выбрал цикл for потому, что извесна последовательность чисел.
+            int initialNumber;
+            int finiteNumber = 97;
+
+            for(initialNumber = 5; initialNumber < finiteNumber; initialNumber+= 7)
+            {
+                Console.WriteLine(initialNumber);
+            }
+        }
+    }
+}
